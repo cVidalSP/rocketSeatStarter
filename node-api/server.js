6efd277902decba322c3e6718e204cba1fa9f0d3
cVidal <kainan.vidal@gmail.com>
2019-05-18 18:44:15 -0300

    Exclusao do node modules, para nao subir novamente, e adicao do nodemon para atualizar a cada mudanca de codigo

--- node-api/server.js
+++ node-api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.send('hello caina');
+    res.send('hello caina, tudo bem?');
 });
 
 app.listen(3001);
