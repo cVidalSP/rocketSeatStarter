188680f0acfd7c8b055799c50a997ead468be5d0
cVidal <kainan.vidal@gmail.com>
2019-05-18 23:11:11 -0300

    Criacao de schemas para o bando, e insert com dados falsos para teste

--- node-api/server.js
+++ node-api/server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 //iniciando o app
 const app = express();
 
 
 //iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi',{ useNewUrlParser: true });
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 //primeira rota
 app.get('/', (req,res)=>{
-    res.send('hello caina, tudo bem?');
+    Product.create({
+        title:'React Native',
+        description:'Build apps with react native',
+        url:'www.google.com.br'
+    })
+
+    return res.send('hello caina, tudo bem?');
 });
 
 app.listen(3001);
