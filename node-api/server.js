fbee06a8079aeccf949172f3a830219a08ff31cc
cVidal <kainan.vidal@gmail.com>
2019-05-19 00:37:01 -0300

    Utilizacao do CORS para que a a API possa ser acessada por outras urls, e finalizacao da API

--- node-api/server.js
+++ node-api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 //iniciando o app
 const app = express();
 app.use(express.json());
+app.use(cors(/* Dominios desejaveis para acesso, opcoes de seguranca e etc */));
 
 
 //iniciando o DB
