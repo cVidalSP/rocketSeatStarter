69c59f33985d87eddb07b89d70d79bf904bbd2b4
cVidal <kainan.vidal@gmail.com>
2019-05-20 23:43:17 -0300

    Estudo sobre a feature de classe em es6, metodos estaticos, heranca e etc

--- curso_es6/main.js
+++ curso_es6/main.js
@@ -1,12 +1,58 @@
-alert('teste');
+// class List{
+//     constructor(){
+//         this.data=[];
+//     }
+//     add(data){
+//         this.data.push(data);
+//         console.log(this.data);
+//     }
+// }
+// // Heranca entre classes
+// class TodoList extends List{
+//     constructor(){
+//         super();// chama o constructor da classe pai
 
-class Teste{
-    metodo(){
+//         this.usuario = "Caina";
+//     }
 
-    }
+//     mostraUsuario(){
+//         console.log(this.usuario);
+//     }
+// }
 
-    outro(){
-        var teste
-        
+// const MinhaLista = new TodoList();
+
+// document.getElementById('novotodo').onclick = function(){
+//     MinhaLista.add('Novo todo');
+// }
+
+// MinhaLista.mostraUsuario();
+
+// TRABALHANDO COM METODOS ESTATICOS
+
+// class TodoList{
+//     constructor(){
+//         this.todos = [];
+//     }
+//     static addTodo(){
+//         this.todos.push('Novo todo');
+//         console.log(this.todos);
+//     }
+
+// }
+
+//     //  Dara erro, pois quando se declara como estatico,
+//     //o metodo nao enxerga a propria classe
+// TodoList.addTodo();
+
+
+// OU SEJA, metodos estaticos devem ser utilizados para realizar acoes
+// para fora do sua classe  por assim dizer. Por exemplo: 
+
+class Matematica{
+    static soma(a,b){
+        return a + b;
     }
 }
+
+console.log(Matematica.soma(1, 2));
