b402fea6dc49d8c40c69fadcfa0db8cb0e546b53
cVidal <kainan.vidal@gmail.com>
2019-06-03 23:22:48 -0300

    Instalacao do axios e uso dele para uma requisao na api do github.

--- curso_es6/src/main.js
+++ curso_es6/src/main.js
@@ -1,53 +1,31 @@
-// Async/await
+import axios from 'axios';
 
-// promise  sao formas de realizar acoes de forma assincrona como solicitacoes para apis
+// class Api{
+//     static async getUserInfo(username){
+//         const response = await axios.get(`https://api.github.com/users/${username}`);
 
-const minhaPromise = () => new Promise((resolve, reject)=>{
-    setTimeout(() => {resolve('OK')}, 1000);
-});
-const minhaPromise2 = () => new Promise((resolve, reject)=>{
-    setTimeout(() => {resolve('OK2')}, 1000);
-});
+//         console.log(response);
+//     }
+// }
 
-// minhaPromise().then(response=>{
-//     console.log(response);
-// })
-// .catch(err =>{
-//     //Capturar os erros.
-// });
+// Api.getUserInfo('cVidalSP'); // Retorna os dados do perfil do git
 
-async function executaPromise(){
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
+//                             // Caso seja invalido, seria usado o catch, mas o async eh usado o try/catch
+//
+// O Axios retorna promises em suas funcoes, por isso eh possivel usar o asyn/await
+class Api{
+    static async getUserInfo(username){
+        try{
+            const response = await axios.get(`https://api.github.com/users/${username}`);
 
-    //Sem o async/await :
-
-    minhaPromise().then(response => {
-        console.log(response);
-
-        minhaPromise().then(response =>{
             console.log(response);
 
-            minhaPromise().then(response =>{
-                console.log(response);
-            })
-        })
-    })
+        } catch(err){
+            console.warn('Erro na API');
+        }
+        
+    }
 }
 
-const executaPromiseComArrowFunct = async () => {
-    console.log(await minhaPromise2());
-    console.log(await minhaPromise2());
-    console.log(await minhaPromise2());
-}
-
-
-// A partir do momento que a funcao eh async, ela vira uma promise
-// porem o "await" so pode ser usado dentro da funcao async
-// tendo assim que se usar o .then
-
-// executaPromise.then(response ...) // Por exemplo
-executaPromise();
-executaPromiseComArrowFunct();
-
+Api.getUserInfo('cVidalSP');
+Api.getUserInfo('cVidalSPasdasd');
