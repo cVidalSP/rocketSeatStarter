27975dda83e55a2630b81315a20e3a99daa549ec
cVidal <kainan.vidal@gmail.com>
2019-06-04 22:25:26 -0300

    Exercicios async/await modulo 3

--- curso_es6/src/main.js
+++ curso_es6/src/main.js
@@ -1,31 +1,130 @@
-import axios from 'axios';
+// Exercicios Modulo 3
 
-// class Api{
-//     static async getUserInfo(username){
-//         const response = await axios.get(`https://api.github.com/users/${username}`);
+// Transformar os trechos de codigo utilizando async/await
 
-//         console.log(response);
-//     }
+
+// Funcão delay aciona o .then após 1s
+// const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+// function umPorSegundo() {
+//     delay().then(() => {
+//         console.log('1s');
+
+//         delay().then(() => {
+//             console.log('2s');
+
+//             delay().then(() => {
+//                 console.log('3s');
+//             });
+//         })
+//     });
 // }
 
-// Api.getUserInfo('cVidalSP'); // Retorna os dados do perfil do git
+// umPorSegundo();
 
-//                             // Caso seja invalido, seria usado o catch, mas o async eh usado o try/catch
-//
-// O Axios retorna promises em suas funcoes, por isso eh possivel usar o asyn/await
-class Api{
-    static async getUserInfo(username){
-        try{
-            const response = await axios.get(`https://api.github.com/users/${username}`);
+// // async function umPorSegundo2(){
+// //     await delay();
+// //         console.log('1s');
+// //     await delay();
+// //         console.log('2s');
+// //     await delay();
+// //         console.log('3s');
+    
+// // }
 
-            console.log(response);
+// // Com arrow function 
+// const umPorSegundo2 = async () =>{
+//     await delay();
+//         console.log('1s');
+//     await delay();
+//         console.log('2s');
+//     await delay();
+//         console.log('3s');
+    
+// }
 
-        } catch(err){
-            console.warn('Erro na API');
-        }
-        
-    }
-}
+// umPorSegundo2();
 
-Api.getUserInfo('cVidalSP');
-Api.getUserInfo('cVidalSPasdasd');
+
+
+
+// import axios from 'axios';
+
+// function getUserFromGithub(user) {
+//     axios.get(`https://api.github.com/users/${user}`)
+//     .then(response => {
+//         console.log(response.data);
+//     })
+//     .catch(err => {
+//         console.log('Usuário não existe');
+//     })
+// }
+// getUserFromGithub('diego3g');
+// getUserFromGithub('diego3g124123');
+
+
+// async function getUserFromGithub2(user) {
+//     try{
+//         const response = await axios.get(`https://api.github.com/users/${user}`);
+
+//         console.log(response);
+//     }catch(err){
+//         console.log('Usuário não existe');
+//     }
+// }
+// getUserFromGithub2('cVidalSP');
+// getUserFromGithub2('cVidalSPASUDHFLKAS');
+
+
+// import axios from 'axios';
+
+// class Github {
+//     static getRepositories(repo) {
+//     axios.get(`https://api.github.com/repos/${repo}`)
+//         .then(response => {
+//            console.log(response.data);
+//         })
+//         .catch(err => {
+//             console.log('Repositório não existe');
+//         })
+//     }
+// }
+// Github.getRepositories('rocketseat/rocketseat.com.br');
+// Github.getRepositories('rocketseat/dslkvmskv');
+
+// class Github2 {
+//     static async getRepositories(repo) {
+//         try{
+//             const response = await axios.get(`https://api.github.com/repos/${repo}`);
+//             console.log(response.data);
+//         }catch(err){
+//             console.log('Repositório não existe');
+//         }
+//     }
+// }
+// Github2.getRepositories('rocketseat/rocketseat.com.br');
+// Github2.getRepositories('rocketseat/dslkvmskv');
+
+
+// const buscaUsuario = user => {
+//     axios.get(`https://api.github.com/users/${user}`)
+//     .then(response => {
+//         console.log(response.data);
+//     })
+//     .catch(err => {
+//         console.warn(err);
+//         console.log('Usuário não existe');
+//     });
+// }
+// buscaUsuario('diego3g');
+
+// const buscaUsuario2 = async(user) => {
+//     try{
+//         const response = await axios.get(`https://api.github.com/users/${user}`);
+//         console.log(response.data);
+//     }catch(err){
+//         console.warn(err);
+//         console.log('Usuário não existe');
+//     }
+// }
+// buscaUsuario2('diego3g');
